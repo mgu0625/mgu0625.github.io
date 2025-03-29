1c365a40c346e396bec3996bb37ef37aa1ca1c11
Monica Aichouri <69013706+mgu0625@users.noreply.github.com>
2025-03-28 20:10:11 -0400

    changed font size on image again, made it smalled

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const NUM_ASCII_COPIES = 3;
-
 fetch('/Attachments/bkgd.txt')
   .then(res => res.text())
   .then(text => {
     const positions = [
-      { top: '15%', left: '30%' }, 
-      { top: '15%', left: '60%' }, 
-      { top: '40%', left: '45%' } 
+      { top: '15%', left: '30%' }, // Left ear
+      { top: '15%', left: '60%' }, // Right ear
+      { top: '40%', left: '45%' }  // Head/center
     ];
 
     positions.forEach(pos => {
@@ -14,7 +12,7 @@ fetch('/Attachments/bkgd.txt')
       asciiEl.className = 'ascii-message';
       asciiEl.style.top = pos.top;
       asciiEl.style.left = pos.left;
-      asciiEl.innerHTML = `<pre style="transform: scale(0.8); transform-origin: top left;">${text}</pre>`;
+      asciiEl.innerHTML = `<pre>${text}</pre>`;
       document.body.appendChild(asciiEl);
     });
   });
