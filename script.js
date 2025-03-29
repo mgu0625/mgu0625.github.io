e8ad8b5071c867566d8043ba8854173793cbdf1f
Monica Aichouri <69013706+mgu0625@users.noreply.github.com>
2025-03-28 20:34:13 -0400

    cut the txt image domn to 1

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 (() => {
     fetch('/Attachments/bkgd.txt')
-        .then(res => res.text())
-        .then(text => {
-        const positions = [
-            { top: '65%', left: '20%' }, // Left
-        ];
-    
-        positions.forEach(pos => {
-            const asciiEl = document.createElement('div');
-            asciiEl.className = 'ascii-message';
-            asciiEl.style.top = pos.top;
-            asciiEl.style.left = pos.left;
-            asciiEl.innerHTML = `<pre>${text}</pre>`;
-            document.body.appendChild(asciiEl);
-        });
-        })
-        .catch(err => {
+      .then(res => res.text())
+      .then(text => {
+        const asciiEl = document.createElement('div');
+        asciiEl.className = 'ascii-message';
+        asciiEl.style.top = '65%';
+        asciiEl.style.left = '20%';
+        asciiEl.innerHTML = `<pre>${text}</pre>`;
+        document.body.appendChild(asciiEl);
+      })
+      .catch(err => {
         console.error("ASCII Load Error:", err);
-        });
-    })();
+      });
+  })();
+  
