30984b48ec6cae18a7931cbdaa5148d9ac025673
Monica Aichouri <69013706+mgu0625@users.noreply.github.com>
2025-03-28 20:15:32 -0400

    testing

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ fetch('/Attachments/bkgd.txt')
   .then(res => res.text())
   .then(text => {
     const positions = [
-      { top: '15%', left: '30%' }, // Left ear
-      { top: '15%', left: '60%' }, // Right ear
-      { top: '40%', left: '45%' }  // Head/center
+      { top: '15%', left: '30%' }, 
+      { top: '15%', left: '60%' }, 
+      { top: '40%', left: '45%' }  
     ];
 
     positions.forEach(pos => {
       const asciiEl = document.createElement('div');
       asciiEl.className = 'ascii-message';
+      asciiEl.style.position = 'absolute';
       asciiEl.style.top = pos.top;
       asciiEl.style.left = pos.left;
       asciiEl.innerHTML = `<pre>${text}</pre>`;
       document.body.appendChild(asciiEl);
+      });
     });
-  });
